8aa503391957fba3cf1ad3f1e6b0f80b2d0d9bd8
David Lekei <DLekei@mpi.mb.ca>
2024-04-19 11:56:40 -0500

    arg parsing works

--- src/ArgParser/ArgParser.go
+++ src/ArgParser/ArgParser.go
@@ -1,14 +1,32 @@
 package argparser
 
-import (
-	"fmt"
-)
-
-type ArgParser struct {
-	options []string
-	// values map[string]string
+type Option struct {
+	Op       string
+	Required bool
+	Callback func(string)
 }
 
-func Parse() {
-	fmt.Println("Parsing...")
+type argparser struct {
+	options map[string]Option
+	//options []Option
+}
+
+func New(options map[string]Option) *argparser {
+	ap := argparser{options: options}
+	return &ap
+}
+
+func (parser argparser) Parse(args []string) {
+
+	for index, arg := range args {
+		_, ok := parser.options[arg]
+
+		if ok {
+			if index+1 < len(args) {
+				parser.options[arg].Callback(args[index+1])
+			} else {
+				panic("Argument not supplied a value")
+			}
+		}
+	}
 }
--- src/DeployJAR.go
+++ src/DeployJAR.go
@@ -7,12 +7,44 @@ import (
 	ap "mpi.mb.ca/DeployJAR/argparser"
 )
 
+var filePath string
+var environment string
+
+func setFilePath(path string) {
+	filePath = path
+}
+
+func setEnvironment(env string) {
+	environment = env
+}
+
+func parseArgs(args []string) {
+	var options = make(map[string]ap.Option)
+
+	options["-f"] = ap.Option{
+		Op:       "-f",
+		Required: true,
+		Callback: setFilePath,
+	}
+
+	options["-e"] = ap.Option{
+		Op:       "-e",
+		Required: true,
+		Callback: setEnvironment,
+	}
+
+	parser := ap.New(options)
+
+	parser.Parse(args)
+}
+
 func main() {
 	args := os.Args[1:]
 
-	fmt.Println("args: ", args)
+	parseArgs(args)
 
-	ap.Parse()
+	fmt.Println("Path to JAR File: ", filePath)
+	fmt.Println("Environment: ", environment)
 
 	fmt.Println("Done")
 }
